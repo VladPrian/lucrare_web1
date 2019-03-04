5b1502456712070ed05c8b878fcffcb5a4b73a41
Prian Vladislav <32475886+VladPrian@users.noreply.github.com>
2019-03-04 08:47:50 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+class worker{
+	string name;
+	string surname;
+	string hourRate;
+	string hours;
+
+	conconstructor(name, surname, hourRate, hours){
+		this.name = name;
+		this.surname = surname;
+		this.hourRate = hourRate;
+		this.hours = hours;
+	}
+}
